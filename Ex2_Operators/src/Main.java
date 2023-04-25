f6c746c615905f506f897ae759b7f014a8c0c4ce
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-04-25 15:47:10 +0700

    Operators

---
+++ Ex2_Operators/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 6;
+
+        //addition
+        System.out.println(num1 + num2); // 18
+
+        //subtraction
+        System.out.println(num1 - num2); // 6
+
+        //multiplication
+        System.out.println(num1 * num2); // 72
+
+        //division
+        System.out.println(num1 / num2); // 2
+
+        //remainder (modulo / modulus);
+        System.out.println(num1 % num2); // 0
+
+
+
+
+
+
+
+    }
+}
