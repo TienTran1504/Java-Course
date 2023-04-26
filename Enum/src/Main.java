6a27f525be2e7fb98a3080b32aa9c31bfa94ddee
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-04-26 16:47:52 +0700

    Enum

---
+++ Enum/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (LoggingLevel state: LoggingLevel.values()){
+            if(state == LoggingLevel.PENDING){
+
+            }
+        }
+        System.out.println("Hello world!");
+    }
+}
